f0e43a16c017569da5c0e5a55afcefae9d034e6b
= <=>
2025-07-28 14:39:13 +0530

    everything set and developerable

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,59 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
+import { prismaClient } from "../lib/db.ts";
 // import cors from 'cors';
 
 async function init() {
   const app = express();
 
   const PORT = Number(process.env.PORT || 8000);
-  app.use(express.json)
+  app.use(express.json);
 
   const gqlServer = new ApolloServer({
-    typeDefs: "",
-    resolvers: {},
+    typeDefs: `
+      type Query {
+        hello:String
+        say(name:String):String
+      }
+      type Mutation{
+        createUser(email:String!,lastName:String!,firstName:String!,password:String!): Boolean
+      }
+    
+    `,
+    resolvers: {
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              password,
+              firstName,
+              lastName,
+              salt: "random_ass",
+            },
+          });
+        },
+      },
+    },
   });
+
   await gqlServer.start();
 
-  app.use("/graphql",expressMiddleware(gqlServer))
+  app.use("/graphql", expressMiddleware(gqlServer));
 
   app.get("/", (req, res) => {
     res.json({
